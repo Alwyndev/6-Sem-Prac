ad89fd77ad64bbb47d39c4c0c202244224e35ad1
MeMeGoD <alwynpaul2004@gmail.com>
2025-01-26 23:35:47 +0530

    made some minor adjustments.

--- Practicals/draft.py
+++ Practicals/draft.py
@@ -1,34 +1,35 @@
 from matplotlib import pyplot as plt
 
+if __name__ == "__main__":  
 
-bub_file = open("bubSort.txt")
-contents = bub_file.read()
-contents = contents.split(" ns\n")
-contents.pop()
-bsort = []
-for i in contents:
-    bsort.append(int(i)//1000)
+    bub_file = open("bubSort.txt")
+    contents = bub_file.read()
+    contents = contents.split(" ns\n")
+    contents.pop()
+    bsort = []
+    for i in contents:
+        bsort.append(int(i)//10000)
 
-runs = list(range(1, len(bsort) + 1))
-
-
-sel_file = open("selSort.txt")
-contents = sel_file.read()
-contents = contents.split(" ns\n")
-contents.pop()
-ssort = []
-for i in contents:
-    ssort.append(int(i)//1000)
-
-runs = list(range(1, len(ssort) + 1))
+    runs = list(range(1, len(bsort) + 1))
 
 
+    sel_file = open("selSort.txt")
+    contents = sel_file.read()
+    contents = contents.split(" ns\n")
+    contents.pop()
+    ssort = []
+    for i in contents:
+        ssort.append(int(i)//10000)
 
-plt.plot(runs, bsort, label="Bubble Sort Swaps", marker ="o")
-plt.plot(runs, ssort, label="Selection Sort Swaps", marker='x')
-plt.xlabel("Run Number")
-plt.ylabel("Time Taken (*1000) ns")
-plt.title("Comparison of Bubble Sort and Selection Sort")
-plt.legend()
-plt.grid()
-plt.show()
+    runs = list(range(1, len(ssort) + 1))
+
+
+
+    plt.plot(runs, bsort, label="Bubble Sort Swaps", marker ="o")
+    plt.plot(runs, ssort, label="Selection Sort Swaps", marker='x')
+    plt.xlabel("Run Number")
+    plt.ylabel("Time Taken (*10000) ns")
+    plt.title("Comparison of Bubble Sort and Selection Sort")
+    plt.legend()
+    plt.grid()
+    plt.show()
