c62beb33761eb79a3b0f7f914244fc72b7de1355
MeMeGoD <alwynpaul2004@gmail.com>
2025-02-02 22:15:50 +0530

    Made some minor changes

--- Practicals/graph2_DAA.py
+++ Practicals/graph2_DAA.py
@@ -6,12 +6,14 @@ if __name__ == "__main__":
     contents = ins_file.read()
     contents = contents.split(" ns\n")
     contents.pop()
+    runs = []
     isort = []
     for i in contents:
         i=i.split(":")
+        runs.append(int(i[0]))
         isort.append(int(i[1])//1000)
 
-    runs = list(range(1, len(isort) + 1))
+    
 
 
     mer_file = open("mergeSortTimes.txt")
