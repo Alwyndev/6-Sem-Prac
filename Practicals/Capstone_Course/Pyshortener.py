b16431b19de7a4db728ab62ed85c95c184319674
Alwyn Paul <alwynpaul2004@gmail.com>
2025-03-27 10:41:56 +0530

    Made some minor changes

--- Practicals/Capstone_Course/Pyshortener.py
+++
@@ -1,6 +0,0 @@
-import pyshorteners
-url = input("Enter the URL to shorten  :  ")
-shortUrl = pyshorteners.Shortener().tinyurl.short(url)
-print(f"Shortened URL : {shortUrl} ")
-
-# print(pyshorteners.Shortener().available_shorteners)
